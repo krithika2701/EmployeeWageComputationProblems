4367a2fb6e1afc8acea1b9ac0b1fc5c7cc119751
krithika2701 <shirisharudrarapu21@gmail.com>
2023-03-22 00:48:30 +0530

    calculating daily employee wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,12 +5,20 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome message for Employee wage computation problems");
 //Check Employee is Present or Absent
+// Calculate DailyEmployee Wage
+// Assume Wage per Hour is 20
+// Assume Full Day Hour is 8
+		int WageperHour = 20;
+		int FullDayHour = 8;
 		double num = Math.random();
 		System.out.println(num);
+
 		int randomCheck = ((int) (num * 10)) % 2;
 		System.out.println("Random check value:" + randomCheck);
 		if (randomCheck == 1) {
 			System.out.println("employee is present");
+			int employeewage = WageperHour * FullDayHour;
+			System.out.println("employee wage is:" + employeewage);
 		} else if (randomCheck == 0) {
 			System.out.println("employee is absent");
 		}
