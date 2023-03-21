3c2f489770cf9a27fdfdb0da59503ffe3d355380
krithika2701 <shirisharudrarapu21@gmail.com>
2023-03-22 00:38:00 +0530

    checking employee is present or not

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,6 +4,15 @@ public class EmployeeWageComputation {
 //Welcome message for Employee wage computation problems
 	public static void main(String[] args) {
 		System.out.println("Welcome message for Employee wage computation problems");
-
+//Check Employee is Present or Absent
+		double num = Math.random();
+		System.out.println(num);
+		int randomCheck = ((int) (num * 10)) % 2;
+		System.out.println("Random check value:" + randomCheck);
+		if (randomCheck == 1) {
+			System.out.println("employee is present");
+		} else if (randomCheck == 0) {
+			System.out.println("employee is absent");
+		}
 	}
 }
