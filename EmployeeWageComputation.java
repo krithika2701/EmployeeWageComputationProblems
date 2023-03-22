7eac8be5fb532a1b80005362c54f2d4254eed4f9
krithika2701 <shirisharudrarapu21@gmail.com>
2023-03-22 13:38:31 +0530

    Add:adding part time employee wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -8,19 +8,27 @@ public class EmployeeWageComputation {
 // Calculate DailyEmployee Wage
 // Assume Wage per Hour is 20
 // Assume Full Day Hour is 8
+//Assume Part time is 4
 		int WageperHour = 20;
 		int FullDayHour = 8;
+		int PartTimeHour = 4;
 		double num = Math.random();
 		System.out.println(num);
 
-		int randomCheck = ((int) (num * 10)) % 2;
+		int randomCheck = ((int) (num * 10)) % 3;
 		System.out.println("Random check value:" + randomCheck);
 		if (randomCheck == 1) {
 			System.out.println("employee is present");
 			int employeewage = WageperHour * FullDayHour;
 			System.out.println("employee wage is:" + employeewage);
-		} else if (randomCheck == 0) {
+		}
+
+		else if (randomCheck == 0) {
 			System.out.println("employee is absent");
+		} else if (randomCheck == 2) {
+			System.out.println("employee is part time " + randomCheck);
+			int employeeWage = WageperHour * PartTimeHour;
+			System.out.println("employee wage is " + employeeWage);
 		}
 	}
 }
